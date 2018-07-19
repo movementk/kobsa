823ee245740241addf7ad477b93729d04e9beb44
kimyh5392 <mk.kimyh@gmail.com>
2018-07-19 18:31:38 +0900

    기본 셋팅 완료.

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="ko">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>한국생물안전협회</title>
+        <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/assets/fontello/css/fontello.css">
+        <link rel="stylesheet" href="/assets/css/default.css">
+    </head>
+    <body>
+        <!-- Header -->
+        <header id="header">
+            <div class="container">
+                aaa
+            </div>
+        </header>
+        
+        <!-- Main Content Start -->
+        <main id="content">
+            <div class="container">
+                aaa
+            </div>
+        </main>
+        
+        <!-- Footer -->
+        <footer id="footer">
+            <div class="container">
+                aaa
+            </div>
+        </footer>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
