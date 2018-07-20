cda8cdcf6e795c911842f8f863aea51c4540813a
kimyh5392 <mk.kimyh@gmail.com>
2018-07-20 18:30:32 +0900

    메인 퍼블리싱 작업중 . . .

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 <link rel="stylesheet" href="/assets/css/main.css">
 </head>
 <body>
@@ -6,19 +7,89 @@
     <!-- Main Content Start -->
     <main id="content">
         <div class="jumbotron">
-            <div class="details">
-
+            <div class="visual-slider">
+                <div class="details">
+                    <img src="/assets/image/main/bg_md_jumbo.jpg" class="d-none d-md-block d-xl-none jumbo-bg img-fluid" alt="">
+                    <img src="/assets/image/main/bg_xl_jumbo.jpg" class="d-none d-xl-block jumbo-bg img-fluid" alt="">
+                    <div class="v-align">
+                        <div class="table-box">
+                            <div>
+                                <small>생물안전분야의 밝은 미래</small>
+                                <h2>
+                                    For a better future in the field of biosecurity
+                                </h2>
+                                <p>
+                                    한국생물안전협회는 국내 생물안전에 대한 법률적
+                                    요구에 대응하고, 선진화를 통하여 관련산업의 지속적
+                                    발전을 위해 힘쓰고 있습니다
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <figure class="d-none d-md-block">
+                    <img src="/assets/image/main/img_md_slider01.jpg" class="d-md-block d-xl-none img-fluid" alt="">
+                    <img src="/assets/image/main/img_xl_slider01.jpg" class="d-none d-xl-block img-fluid" alt="">
+                </figure>
             </div>
-            <div class="jumbo-img">
-
+            <div class="visual-slider">
+                <div class="details">
+                    <img src="/assets/image/main/bg_md_jumbo.jpg" class="d-none d-md-block d-xl-none jumbo-bg img-fluid" alt="">
+                    <img src="/assets/image/main/bg_xl_jumbo.jpg" class="d-none d-xl-block jumbo-bg img-fluid" alt="">
+                    <div class="v-align">
+                        <div class="table-box">
+                            <div>
+                                <small>생물안전분야의 밝은 미래</small>
+                                <h2>
+                                    For a better future in the field of biosecurity
+                                </h2>
+                                <p>
+                                    한국생물안전협회는 국내 생물안전에 대한 법률적
+                                    요구에 대응하고, 선진화를 통하여 관련산업의 지속적
+                                    발전을 위해 힘쓰고 있습니다
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <figure class="d-none d-md-block">
+                    <img src="/assets/image/main/img_md_slider01.jpg" class="d-md-block d-xl-none img-fluid" alt="">
+                    <img src="/assets/image/main/img_xl_slider01.jpg" class="d-none d-xl-block img-fluid" alt="">
+                </figure>
+            </div>
+            <div class="visual-slider">
+                <div class="details">
+                    <img src="/assets/image/main/bg_md_jumbo.jpg" class="d-none d-md-block d-xl-none jumbo-bg img-fluid" alt="">
+                    <img src="/assets/image/main/bg_xl_jumbo.jpg" class="d-none d-xl-block jumbo-bg img-fluid" alt="">
+                    <div class="v-align">
+                        <div class="table-box">
+                            <div>
+                                <small>생물안전분야의 밝은 미래</small>
+                                <h2>
+                                    For a better future in the field of biosecurity
+                                </h2>
+                                <p>
+                                    한국생물안전협회는 국내 생물안전에 대한 법률적
+                                    요구에 대응하고, 선진화를 통하여 관련산업의 지속적
+                                    발전을 위해 힘쓰고 있습니다
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <figure class="d-none d-md-block">
+                    <img src="/assets/image/main/img_md_slider01.jpg" class="d-md-block d-xl-none img-fluid" alt="">
+                    <img src="/assets/image/main/img_xl_slider01.jpg" class="d-none d-xl-block img-fluid" alt="">
+                </figure>
             </div>
         </div>
         <div class="container">
-
+            
         </div>
     </main>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script>
         (function($){
             // mobile nav 관련
@@ -56,6 +127,29 @@
                 }
             });
 
+            // visual slider
+            $(window).ready(function(){
+                $('.jumbotron').slick({
+                    fade: true,
+                    infinite: true,
+                    speed: 1000,
+                    arrows: true,
+                    dots: true,
+                    prevArrow: '<button class="slick-prev" type="button"><i class="icon-left"></i></button>',
+                    nextArrow: '<button class="slick-next" type="button"><i class="icon-right"></i></button>'
+                });
+            });
+            $('.jumbotron').on('afterChange', function(event, slick, currentSlide, nextSlide){
+                $('.slick-slide.visual-slider').removeClass('action');
+                setTimeout(function (){ 
+                    $('.slick-active.visual-slider').addClass('action'); 
+                });
+            });
+            $('.jumbotron').on('init', function(event,slick){
+                setTimeout(function(){ 
+                    $('.slick-active.visual-slider').addClass('action'); 
+                });
+            });
         })(jQuery);
     </script>
 </body>
