7542546924e7d30c301aefe2a9093b4f323e7e47
kimyh5392 <mk.kimyh@gmail.com>
2018-07-27 23:01:49 +0900

    greeting 작업중 . . .

---
+++ introduce/greeting.php
@@ -0,0 +1,70 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+<link rel="stylesheet" href="/assets/css/introduce.css">
+</head>
+<body class="sub introduce">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    
+    <!-- page-header -->
+    <div class="page-header">
+        <small>협회소개</small>
+        <h3 class="page-title">ASSOCIATION<br class="d-md-none"> INTRODUCE</h3>
+    </div>
+    
+    <!-- Sub Content Start -->
+    <main id="content">
+        <div class="container">
+            <!-- tab-list -->
+            <div class="lnb">
+                <ul>
+                    <li class="active"><a href="/introduce/greeting.php">협회장 인사말</a></li>
+                    <li><a href="#">협회소개</a></li>
+                    <li><a href="#">회원사소개</a></li>
+                    <li><a href="#">조직구성</a></li>
+                    <li><a href="#">주요사업</a></li>
+                    <li><a href="#">찾아오시는길</a></li>
+                </ul>
+            </div>
+            
+            <section class="greeting">
+                <div class="section-header">
+                    <h4>협회장 인사말</h4>
+                </div>
+                <div class="section-content">
+                    <article class="president">
+                        <div class="article-header">
+                            
+                        </div>
+                        <div class="article-content">
+                            <div class="row">
+                                <p class="col-12 col-xl-6">
+                                    2011년부터 협회에서 운영하는 생물안전관리자 및 생물안전전문가 민간자격제도를 통하여 생물안전분야 종사자 전문교육 및 자격인증을 실시하고 있으며, 100명이 넘는 생물안전관리자를 배출하였습니다. 또한 질병관리본부 유전자변형생물체 안전관리 교육기관으로 지정되어 lmo법에 따른 생물안전 2,3 등급 생물안전관리자를 비롯하여 관계자 교육을 진행합니다.<br><br>
+                                    매년 2월에 열리는 한국생물안전협회 정기총회 및 생물안전연구시설 설치, 운영자 워크숍과 하반기한국생물안전컨퍼런스를 통하여 국내 생물안전분야 최신 기술 및 동향을 공유하고 전문가와 종사자가교류하는 자리를 마련하고 있습니다.
+                                </p>
+                                <p class="col-12 col-xl-6">
+                                    향후 생물안전 관련 기업의 전문성과 안전성을 보장하기 위하여 세부 분야별로 기준을 마련하여 기술력을 인증하는 사업을 추진하고자 하며, 이를 통하여 유능하고 검증된 전문기업을 생물안전 시설관계자에게소개하고 국제 기준에 부합하는 안전성과 전문성을 갖추도록 역활을 할 것입니다. <br><br>
+                                    한국생물안전협회는 그동안 김찬화, 홍진관, 이희찬 역대 회장님들을 비롯하여 많은 회원들과 각계 전문가들의 도움으로 지속적인 발전을 해왔으며, 앞으로도 국내 생물안전 민간부분을 책임지는 유일한단체로 생물안전과 생물보안 산업의 발전과 기술 향상을 위해 다양한 할동을 벌여 나갈 것입니다. <br><br>
+                                    <b>회원님들의 적극적인 참여와 격려를 부탁드립니다.</b>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                    <hr>
+                    <article class="activity">
+                        <div class="article-header">
+                            
+                        </div>
+                        <div class="article-content">
+                            
+                        </div>
+                    </article>
+                </div>
+            </section>
+        </div>
+    </main>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script type="text/javascript" src="/assets/js/common.js"></script>
+</body>
+</html>
