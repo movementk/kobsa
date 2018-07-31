d4678bc77002ff1af9260905f3117898b6108cac
kimyh5392 <mk.kimyh@gmail.com>
2018-07-31 09:32:49 +0900

    회장 인사말 퍼블리싱 완료

--- introduce/greeting.php
+++ introduce/greeting.php
@@ -33,15 +33,43 @@
                 <div class="section-content">
                     <article class="president">
                         <div class="article-header">
-                            
+                            <div class="president-greeting">
+                                <div class="row">
+                                    <div class="col-12 col-md-6 offset-md-1">
+                                        <div class="summary">
+                                            <figure class="logo">
+                                                <img src="/assets/images/introduce/logo.png" alt="kobsa logo">
+                                            </figure>
+                                            <h5>
+                                                안녕하세요.<br>
+                                                한국생물안전협회장 장원종입니다.
+                                            </h5>
+                                            <p>
+                                                존경하는 한국생물안전협회 회원과 생물안전 분야에 관심을 갖고 홈페이지를 방문해주시는 모든 분들께 감사의 말씀을 드리며 항상 행복이 가득하시고, 소망시하는 바대로 모든 일이 이루어지길 기원합니다
+                                            </p>
+                                            <dl>
+                                                <dt>사단법인 한국생물안전협회</dt>
+                                                <dd>
+                                                    <img src="/assets/images/introduce/img_signature.png" alt="">
+                                                </dd>
+                                            </dl>
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-md-5">
+                                        <figure class="president-img">
+                                            <img src="/assets/images/introduce/img_president.png" class="img-fluid" alt="">
+                                        </figure>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="article-content">
                             <div class="row">
-                                <p class="col-12 col-xl-6">
+                                <p class="col-12 col-xl-6 space">
                                     2011년부터 협회에서 운영하는 생물안전관리자 및 생물안전전문가 민간자격제도를 통하여 생물안전분야 종사자 전문교육 및 자격인증을 실시하고 있으며, 100명이 넘는 생물안전관리자를 배출하였습니다. 또한 질병관리본부 유전자변형생물체 안전관리 교육기관으로 지정되어 lmo법에 따른 생물안전 2,3 등급 생물안전관리자를 비롯하여 관계자 교육을 진행합니다.<br><br>
                                     매년 2월에 열리는 한국생물안전협회 정기총회 및 생물안전연구시설 설치, 운영자 워크숍과 하반기한국생물안전컨퍼런스를 통하여 국내 생물안전분야 최신 기술 및 동향을 공유하고 전문가와 종사자가교류하는 자리를 마련하고 있습니다.
                                 </p>
-                                <p class="col-12 col-xl-6">
+                                <p class="col-12 col-xl-6 space">
                                     향후 생물안전 관련 기업의 전문성과 안전성을 보장하기 위하여 세부 분야별로 기준을 마련하여 기술력을 인증하는 사업을 추진하고자 하며, 이를 통하여 유능하고 검증된 전문기업을 생물안전 시설관계자에게소개하고 국제 기준에 부합하는 안전성과 전문성을 갖추도록 역활을 할 것입니다. <br><br>
                                     한국생물안전협회는 그동안 김찬화, 홍진관, 이희찬 역대 회장님들을 비롯하여 많은 회원들과 각계 전문가들의 도움으로 지속적인 발전을 해왔으며, 앞으로도 국내 생물안전 민간부분을 책임지는 유일한단체로 생물안전과 생물보안 산업의 발전과 기술 향상을 위해 다양한 할동을 벌여 나갈 것입니다. <br><br>
                                     <b>회원님들의 적극적인 참여와 격려를 부탁드립니다.</b>
@@ -52,10 +80,38 @@
                     <hr>
                     <article class="activity">
                         <div class="article-header">
-                            
+                            <h6>협회 활동</h6>
                         </div>
                         <div class="article-content">
-                            
+                            <p>
+                                한국생물안전협회는 생물안전 및 생물보안에 대한 국제적 요구와 [유전자변형생물체의 국가간 이동에 관한 법률] 및 [감염병 예방 및 관리에 관한 법률] 등 국내 생물안전에 대한 법률적 요구에 대응하고, 민관 협력에 의한 생물안전분야의 선진화를 통하여 관련 산업의 지속적 발전과 이의 토대가 되는 학문적 발전을 목적으로 보건복지부의 인가를 받아 2009년 설립되어 다음과 같은 영역에서 활동을 해오고 있습니다.
+                            </p>
+                            <ol>
+                                <li>
+                                    <i>1</i>
+                                    생물안전관리 (책임)자 교육 및 양성
+                                </li>
+                                <li>
+                                    <i>2</i>
+                                    유전자변형생물체 및 고위험병원체의 생물안전 시설 설계, 시공, 유관리, 컨설팅 업무
+                                </li>
+                                <li>
+                                    <i>3</i>
+                                    유전자변형생물체 및 고위험병원체의 생물안전 및 생물보완 교육
+                                </li>
+                                <li>
+                                    <i>4</i>
+                                    유전자변형생물체 및 고위험병원체의 생물안전 및 생물보완 정책 제안
+                                </li>
+                                <li>
+                                    <i>5</i>
+                                    해외 생물안전협회 연계협력 및 교류
+                                </li>
+                                <li>
+                                    <i>6</i>
+                                    생물안전 및 생물보안에 관한 정부 위탁사업 등
+                                </li>
+                            </ol>
                         </div>
                     </article>
                 </div>
