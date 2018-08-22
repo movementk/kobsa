7998e7116c8ebb617f001483166194cea8c3ba69
Joahra <mk_ahra0522@gmail.com>
2018-08-22 18:29:21 +0900

    주요사업 작업중... 50%

---
+++ introduce/government.php
@@ -0,0 +1,112 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" href="/assets/css/sub.css">
+<link rel="stylesheet" href="/assets/css/introduce.css">
+</head>
+<body class="sub introduce">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <!-- page-header -->
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/introduce/_pageheader.php'); ?>
+    
+    <!-- Sub Content Start -->
+	<main id="content">
+		<div class="container">
+			<!-- lnb-list -->
+			<div class="lnb-list">
+				<ul>
+					<li><a href="/introduce/greeting.php">협회장 인사말</a></li>
+					<li><a href="/introduce/association.php">협회소개</a></li>
+					<li><a href="/introduce/member.php">회원사소개</a></li>
+					<li><a href="/introduce/organization.php">조직구성</a></li>
+					<li class="active"><a href="/introduce/professional.php">주요사업</a></li>
+					<li><a href="#">찾아오시는길</a></li>
+				</ul>
+			</div>
+		</div>
+		<section class="government">
+			<div class="container">
+				<div class="section-header">
+					<h4>주요사업</h4>
+				</div>
+				<div class="section-content">
+					<nav class="tab-wrap">
+						<div class="tab-menu">
+							<ul>
+								<li><a href="/introduce/professional.php">생물안전교육</a></li>
+								<li class="active"><a href="#">생물안전관리</a></li>
+							</ul>
+						</div>
+						<div class="sub-menu">
+							<ul>
+								<li><a href="/introduce/private.php">민간용역사업</a></li>
+								<li class="on"><a href="/introduce/government.php">정부용역사업</a></li>
+								<li><a href="#">국제용역사업</a></li>
+								<li><a href="#">기업인증기준 제정</a></li>
+							</ul>
+						</div>
+					</nav>
+					<article>
+						<div class="article-header">
+							<h3 class="sub-title">정부용역사업</h3>
+						</div>
+						<div class="article-content">
+							<ol class="number-list">
+								<li>
+									<span>1</span>
+									<h3>질병관리본부 “생물안전 및 생물보완 인식 확산을 위한 교육체계 개발 연구” 용역</h3>
+									<small> (2013년 2월 15일~11월 24일)</small>
+								</li><li>
+									<span>2</span>
+									<h3>질병관리본부 “시·도보건환경연구원 BL3 연구시설 시공·검증을 위한 현장 컨설팅” 용역</h3>
+									<small> (2011년 9월 7일~2012년 8월 2일)  </small>
+								</li>
+								<li>
+									<span>3</span>
+									<h3>질병관리본부 “생물안전4등급 연구시설 설치·운영 상세기준 및 검증 규격 개발 연구” 용역 </h3>
+									<small>
+										(2011년 3월 2일~12월 1일)  
+									</small>
+								</li>
+								<li>
+									<span>4</span>
+									<h3>질병관리본부 “생물안전 연구시설 설치·검증 컨설팅 체계 선진화 방안 연구” 용역</h3>
+									<small>
+										(2011년 3월 3일~9월 2일)  
+									</small>
+								</li>
+								<li>
+									<span>5</span>
+									<h3>질병관리본부 “시·도보건환경연구원 BL3 연구시설 설계 표준 작업지침서 개발 및 컨설팅” 용역</h3>
+									<small>(2010년 11월 19일~5월 18일)</small>
+								</li>
+								<li>
+									<span>6</span>
+									<h3>농림축산검역본부 “생물안전 3등급 연구시설 설계시공 컨설팅 및 인허가 지원과 생물안전운영지침 개발”</h3>
+									<small> (2013년 7월 4일 ~ 2014년 11월 30일)  </small>
+								</li>
+								<li>
+									<span>7</span>
+									<h3>농림축산검역본부 “식물검역병해충 생물안전검사동 신축 타당성 조사 및 기본계획 수립”</h3>
+									<small>(2014년 3월 31일 ~ 2014년 7월 31일) </small>
+								</li>
+								<li>
+									<span>8</span>
+									<h3>질병관리본부 “생물안전 3등급 연구시설의 폐기 및 재활용을 위한 제독방안연구” </h3>
+									<small>(2015년 11월 ~ 2016년 2월) </small>
+								</li>
+								<li>
+									<span>9</span>
+									<h3>식품의약품안전평가원 “병원성 미생물의 생물안전등급(BSL)에 따른 백신 제조 및 품질관리시설의 기준 개발 연구” </h3>
+									<small>(2016년 6월 1일 ~ 2017년 3월31일) </small>
+								</li>
+							</ol>
+						</div>
+					</article>
+				</div>
+			</div>
+		</section>
+	</main>
+    
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
