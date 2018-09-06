7f4b2f1aede4bfcca50db94c464414c561f81f33
kimyh5392 <mk.kimyh@gmail.com>
2018-09-06 16:18:22 +0900

    index2.php 새로 작업

---
+++ index2.php
@@ -0,0 +1,266 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+<link rel="stylesheet" href="/assets/css/main.css">
+</head>
+<body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <!-- Main Content Start -->
+    <main id="content">
+        <div class="jumbotron">
+            <div class="visual-slider">
+                <div class="summary">
+                    <div class="bg-box d-block d-xl-none" style="background-image: url(/assets/images/main/bg_md_jumbo.jpg);"></div>
+                    <div class="bg-box d-none d-xl-block" style="background-image: url(/assets/images/main/bg_xl_jumbo.jpg);"></div>
+                    <div class="slogan">
+                        <small>생물안전분야의 밝은 미래</small>
+                        <h3>For a better future in the field of biosecurity</h3>
+                        <p>
+                            한국생물안전협회는 국내 생물안전에 대한 법률적
+                            요구에 대응하고, 선진화를 통하여 관련산업의 지속적
+                            발전을 위해 힘쓰고 있습니다
+                        </p>
+                    </div>
+                </div>
+                <div class="slider-bg d-block d-xl-none" style="background-image: url(/assets/images/main/img_md_slider01.jpg);"></div>
+                <div class="slider-bg d-none d-xl-block" style="background-image: url(/assets/images/main/img_xl_slider01.jpg);"></div>
+            </div>
+            <div class="visual-slider">
+                <div class="summary">
+                    <div class="bg-box d-block d-xl-none" style="background-image: url(/assets/images/main/bg_md_jumbo.jpg);"></div>
+                    <div class="bg-box d-none d-xl-block" style="background-image: url(/assets/images/main/bg_xl_jumbo.jpg);"></div>
+                    <div class="slogan">
+                        <small>생물안전분야의 밝은 미래</small>
+                        <h3>For a better future in the field of biosecurity</h3>
+                        <p>
+                            한국생물안전협회는 국내 생물안전에 대한 법률적
+                            요구에 대응하고, 선진화를 통하여 관련산업의 지속적
+                            발전을 위해 힘쓰고 있습니다
+                        </p>
+                    </div>
+                </div>
+                <div class="slider-bg d-block d-xl-none" style="background-image: url(/assets/images/main/img_md_slider01.jpg);"></div>
+                <div class="slider-bg d-none d-xl-block" style="background-image: url(/assets/images/main/img_xl_slider01.jpg);"></div>
+            </div>
+            <div class="visual-slider">
+                <div class="summary">
+                    <div class="bg-box d-block d-xl-none" style="background-image: url(/assets/images/main/bg_md_jumbo.jpg);"></div>
+                    <div class="bg-box d-none d-xl-block" style="background-image: url(/assets/images/main/bg_xl_jumbo.jpg);"></div>
+                    <div class="slogan">
+                        <small>생물안전분야의 밝은 미래</small>
+                        <h3>For a better future in the field of biosecurity</h3>
+                        <p>
+                            한국생물안전협회는 국내 생물안전에 대한 법률적
+                            요구에 대응하고, 선진화를 통하여 관련산업의 지속적
+                            발전을 위해 힘쓰고 있습니다
+                        </p>
+                    </div>
+                </div>
+                <div class="slider-bg d-block d-xl-none" style="background-image: url(/assets/images/main/img_md_slider01.jpg);"></div>
+                <div class="slider-bg d-none d-xl-block" style="background-image: url(/assets/images/main/img_xl_slider01.jpg);"></div>
+            </div>
+        </div>
+        
+        <!-- professional-list -->
+        <article class="professional-list">
+            <div class="container">
+                <ul class="row">
+                    <li class="col-6 col-lg-3">
+                        <div class="professional plan">
+                            <h4>설계, 시공 전문</h4>
+                            <small>(실적소개-기업별)</small>
+                            <p class="d-none d-lg-block">설계, 시공 전문 기업들의 실적과 성과를 소개합니다.</p>
+                            <div class="more">
+                                <a href="#" class="btn btn-more" role="button">자세히 보기</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="col-6 col-lg-3">
+                        <div class="professional facility">
+                            <h4>시설 유지보수전문</h4>
+                            <small>(실적소개-기업별)</small>
+                            <p class="d-none d-lg-block">전문적인 시설 유지보수 업무로 축적된 영량을 바탕으로 고객편의를 위한 서비스를 제공합니다.</p>
+                            <div class="more">
+                                <a href="#" class="btn btn-more" role="button">자세히 보기</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="col-6 col-lg-3">
+                        <div class="professional equipment">
+                            <h4>장비 및 소모품 전문</h4>
+                            <small>(실적소개-제품별)</small>
+                            <p class="d-none d-lg-block">앞선장비와 소모품등 다양한 제품들의 정보를 제공합니다</p>
+                            <div class="more">
+                                <a href="#" class="btn btn-more" role="button">자세히 보기</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="col-6 col-lg-3">
+                        <div class="professional sterilize">
+                            <h4>멸균 및 검증 전문</h4>
+                            <small>(실적소개-기업별)</small>
+                            <p class="d-none d-lg-block">우수한 멸균성을 보장하고 모든 안전 규범을 준수할 수 있도록 도와드립니다.</p>
+                            <div class="more">
+                                <a href="#" class="btn btn-more" role="button">자세히 보기</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </article>
+        
+        <!-- about -->
+        <section class="about">
+            <div class="container">
+                <div class="section-content">
+                    <div class="row">
+                        <div class="col-12 col-xl-4">
+                            <div class="about-head">
+                                <h3><small>About KOBSA</small>Who are we?</h3>
+                                <p class="summary">
+                                    생물안전분야의 선진화를 통하여 관련산업의<br class="d-md-none d-xl-block">
+                                    지속적 발전을 위해 힘쓰고 있습니다
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-xl-6 offset-xl-1">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_about01.jpg" alt="">
+                                            <figcaption>생물안전교육</figcaption>
+                                        </figure>
+                                        <p class="more">자세히 보기</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_about02.jpg" alt="">
+                                            <figcaption>생물안전관리</figcaption>
+                                        </figure>
+                                        <p class="more">자세히 보기</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <figure>
+                                            <img src="/assets/images/main/img_about03.jpg" alt="">
+                                            <figcaption>생물안전협의회</figcaption>
+                                        </figure>
+                                        <p class="more">자세히 보기</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+        
+        <!-- kobas-news -->
+        <section class="kobas-news">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-4">
+                        <article class="notice">
+                            <h3><a href="#">공지사항<i></i></a></h3>
+                            <ul>
+                                <li><a href="#">2018년 생물안전 2,3등급연구시설 생물안전관리자(책임2018년 생물안전 2,3등급연구시설 생물안전관리자(책임</a></li>
+                                <li><a href="#">2018년 생물안전 2,3등급연구시설 생물안전관리자(책임</a></li>
+                            </ul>
+                        </article>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <article class="board">
+                            <h3><a href="#">게시판<i></i></a></h3>
+                            <ul>
+                                <li><a href="#">2018년 생물안전 2,3등급연구시설 생물안전관리자(책임2018년 생물안전 2,3등급연구시설 생물안전관리자(책임</a></li>
+                                <li><a href="#">2018년 생물안전 2,3등급연구시설 생물안전관리자(책임</a></li>
+                            </ul>
+                        </article>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <article class="qna">
+                            <h3><a href="#">Q&amp;A<i></i></a></h3>
+                            <ul>
+                                <li><a href="#">2018년 생물안전 2,3등급연구시설 생물안전관리자(책임2018년 생물안전 2,3등급연구시설 생물안전관리자(책임</a></li>
+                                <li><a href="#">2018년 생물안전 2,3등급연구시설 생물안전관리자(책임</a></li>
+                            </ul>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <!-- partner-list -->
+        <article class="partner-list">
+            <div class="container">
+                <ul class="partner-slider row">
+                    <li class="col-6 col-md-4 col-xl-3">
+                        <a href="#" target="_blank">
+                            <figure>
+                                <img src="/assets/images/main/img_partner01.jpg" class="img-fluid" alt="">
+                            </figure>
+                        </a>
+                    </li>
+                    <li class="col-6 col-md-4 col-xl-3">
+                        <a href="#" target="_blank">
+                            <figure>
+                                <img src="/assets/images/main/img_partner02.jpg" class="img-fluid" alt="">
+                            </figure>
+                        </a>
+                    </li>
+                    <li class="col-6 col-md-4 col-xl-3">
+                        <a href="#" target="_blank">
+                            <figure>
+                                <img src="/assets/images/main/img_partner03.jpg" class="img-fluid" alt="">
+                            </figure>
+                        </a>
+                    </li>
+                    <li class="col-6 col-md-4 col-xl-3">
+                        <a href="#" target="_blank">
+                            <figure>
+                                <img src="/assets/images/main/img_partner04.jpg" class="img-fluid" alt="">
+                            </figure>
+                        </a>
+                    </li>
+                    <li class="col-6 col-md-4 col-xl-3">
+                        <a href="#" target="_blank">
+                            <figure>
+                                <img src="/assets/images/main/img_partner01.jpg" class="img-fluid" alt="">
+                            </figure>
+                        </a>
+                    </li>
+                    <li class="col-6 col-md-4 col-xl-3">
+                        <a href="#" target="_blank">
+                            <figure>
+                                <img src="/assets/images/main/img_partner02.jpg" class="img-fluid" alt="">
+                            </figure>
+                        </a>
+                    </li>
+                    <li class="col-6 col-md-4 col-xl-3">
+                        <a href="#" target="_blank">
+                            <figure>
+                                <img src="/assets/images/main/img_partner03.jpg" class="img-fluid" alt="">
+                            </figure>
+                        </a>
+                    </li>
+                    <li class="col-6 col-md-4 col-xl-3">
+                        <a href="#" target="_blank">
+                            <figure>
+                                <img src="/assets/images/main/img_partner04.jpg" class="img-fluid" alt="">
+                            </figure>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </article>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script type="text/javascript" src="/assets/js/main.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+</body>
+</html>
